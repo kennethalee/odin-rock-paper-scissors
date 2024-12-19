585cb80f91d6c3aeab99f98bba1a87b2d4318343
Kenneth Aidan Lee <158255466+kennethalee@users.noreply.github.com>
2024-12-19 12:09:54 +0900

    Finalize working logic for game mechanics

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-humanScore = 0
-computerScore = 0
+let humanScore = 0
+let computerScore = 0
 
-function getComputerChoice(num=3) {
-    let random_number = Math.floor(Math.random() * num);
-    if (random_number === 0) {
-        console.log("Computer chose: Rock");
-    } else if (random_number === 1) {
-        console.log("Computer chose: Paper");
-    } else {
-        console.log("Computer chose: Scissors");
-    }
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(`Computer chose: ${randomChoice}`);
+    return randomChoice;
 }
 
 function getHumanChoice() {
-    let human_choice = prompt("Choose you weapon: ")
-    return `You chose: ${human_choice}`;
+    let human_choice = prompt("Choose your weapon: ").toLowerCase();
+    console.log(`You chose: ${human_choice}`);
+    return human_choice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase()
+    if (
+        humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        humanScore += 1;
+        return "You Win!";
+    } else if (
+        humanChoice === "rock" && computerChoice === "paper" ||
+        humanChoice === "paper" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        computerScore += 1;
+        return "You Lose!";
+    } else if (humanChoice === computerChoice) {
+        return "It's a Tie!"
+    } else {
+        return "Invalid input!"
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+const result = playRound(humanSelection, computerSelection);
+
+console.log(result);
+console.log(`Scores: Human - ${humanScore}, Computer - ${computerScore}`);
